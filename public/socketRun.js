b938b08fd14f1bc927b21f9271f0555c7ca9961e
rmt002 <roshanmthaliath@gmail.com>
2019-05-29 23:02:41 +0530

    old changes

--- public/socketRun.js
+++ public/socketRun.js
@@ -1,5 +1,5 @@
 $(function () {
-    var socket = io.connect('http://localhost:3000')
+    var socket = io.connect('https://betchat-velunmxwgs.now.sh')
     var message = $("#message")
     var username = $("#username")
     var sendMessage = $("#send_message")
