4d4598a868b11ba0c33ed801806c5178ab1ed4c3
rmt002 <roshanmthaliath@gmail.com>
2019-05-31 22:55:13 +0530

    Final Changes to Socket Server

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express=require('express')
+const express=require('express');
 const app=express();
-const port=5000
+const port=5000;
 
 //app.use('/favicon.ico', express.static('images/favicon.ico'));
 
@@ -15,22 +15,11 @@ server=app.listen(port,(res)=>{
 const io=require("socket.io")(server)
 
 io.on('connection',(socket)=>{
-    socket.username="ANON"
-    console.log('Requesting Connection..')
-    socket.on('change_username',(obj)=>{
-        socket.username=obj.username
-    })
-    var testvalue=2000
-    socket.on('testConnection',(data)=>{
-        console.log("connection has been estabilished",data)
-        socket.emit("res",testvalue)
+    socket.on('clientSend',(data)=>{
+        emittor(data)
     })
 
-    socket.on("new_message", (data)=>{
-        console.log('new message recieved')
-        io.sockets.emit('new_message',{
-            message:data.message,
-            username:socket.username
-        })
-    })
+    emittor=(data)=>{
+        socket.emit("serverSend",data)
+    }
 })
