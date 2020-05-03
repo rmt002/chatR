51e581a441230411f2d082df3292f9ca1ac1d69f
rmt002 <roshanmthaliath@gmail.com>
2020-05-03 12:47:07 +0530

    Updating node dependencies

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const express=require('express');
-const app=express();
-const port=5000;
+const express = require('express');
+const app = express();
+const port = 5000;
 
 //app.use('/favicon.ico', express.static('images/favicon.ico'));
 
-app.get('/',(req,res)=>{
-    res.send("The server is running")
+app.get('/', (req, res) => {
+    res.send("The socket server is running")
 })
 
-server=app.listen(port,(res)=>{
-    console.log("App is running on port : "+ port)
+server = app.listen(port, (res) => {
+    console.log("Socket app is running on port : " + port)
 })
 
-const io=require("socket.io")(server)
+const io = require("socket.io")(server)
 
-io.on('connection',(socket)=>{
-    socket.on('clientSend',(data)=>{
+io.on('connection', (socket) => {
+    socket.on('clientSend', (data) => {
         emittor(data)
     })
 
-    emittor=(data)=>{
-        socket.emit("serverSend",data) //emit to the client that sent the message
-        socket.broadcast.emit("serverSend",data) //emit to everyone other than that client
+    emittor = (data) => {
+        socket.emit("serverSend", data) //emit to the client that sent the message
+        socket.broadcast.emit("serverSend", data) //emit to everyone other than that client
     }
 })
