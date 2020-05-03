f17d92797be3c86554490ba1f3b761921ac1fcd4
rmt002 <roshanmthaliath@gmail.com>
2020-05-03 13:15:57 +0530

    Updated dependencies

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,16 @@
 import openSocket from '../../node_modules/socket.io-client';
-export const url='https://betchat-ovvvixdtsw.now.sh';
-const socket=openSocket(url)
+//export const url='https://betchat-ovvvixdtsw.now.sh';
+export const url = 'http://52.66.149.32:5000/';
+const socket = openSocket(url)
 
-export class MessagingSocket{
-    listen=()=>{
-        socket.on('serverSend',(data)=>{
+export class MessagingSocket {
+    listen = () => {
+        socket.on('serverSend', (data) => {
         })
     }
 
-    transmit=(send)=>{    
-        socket.emit('clientSend',send,(data)=>{
+    transmit = (send) => {
+        socket.emit('clientSend', send, (data) => {
         })
     }
 }
